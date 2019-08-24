44d1a7bfe53ae69320b51696e73bf9c40530b4e7
Jhonny Mata <solucionesmatajm@gmail.com>
2019-08-24 14:51:43 -0400

    agregado prueba directorio del archivo

--- test/package.spec.js
+++ test/package.spec.js
@@ -1,4 +1,5 @@
 var assert = require("assert"),
+    path = require("path"),
     Package = require("../src/package")
 
 
@@ -8,12 +9,15 @@ describe("Package", function () {
             version: "2.2.53-1",
             filename: "acl-2.2.53-1-x86_64.pkg.tar.xz",
             md5: "aaaea535e603f2b55cb320a42cc70397",
-            arch: "x86_64"
+            arch: "x86_64",
+            path: path.resolve("./test/repo/")
         }
         var pack = new Package("acl", options)
         assert.equal(pack.filename, options.filename)
         assert.equal(pack.version, options.version)
         assert.equal(pack.md5, options.md5)
         assert.equal(pack.arch, options.arch)
+        assert.equal(pack.path, path.join(options.path, options.filename))
+        assert.equal(pack.pathSig, path.join(options.path, options.filename + ".sig"))
     })
 })
