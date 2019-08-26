e46d9c5da21e94b4fcc7f5498d17aa09d9c2fb57
Jhonny Mata <solucionesmatajm@gmail.com>
2019-08-26 09:40:02 -0400

    agregado pruebas descarga de archivos corruptos

--- test/package.spec.js
+++ test/package.spec.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const assert = require('assert')
 const path = require('path')
 const Package = require('../src/package')
@@ -26,7 +27,7 @@ describe('Package', function () {
         var pack = new Package(packageName, options)
         pack.checkSum(function (error, hash) {
             if (error) {
-                done(error)
+                return done(error)
             }
             assert.strictEqual(hash, pack.md5)
             done()
@@ -57,11 +58,10 @@ describe('Package', function () {
     it('download package', function (done) {
         var baseURI = '/repo/x86_64/'
         var responseContent = 'hola'
-        var opt = Object.assign({
-            path: path.resolve('/tmp/'),
-            md5: '4d186321c1a7f0f354b297e8914ab240',
-            mirror: mirror + 'repo/x86_64'
-        }, options)
+        var opt = Object.assign({}, options)
+        opt.path = path.resolve('/tmp/')
+        opt.md5 = '4d186321c1a7f0f354b297e8914ab240'
+        opt.mirror = mirror + 'repo/x86_64'
         nock(mirror)
             .get(baseURI + opt.filename)
             .reply(200, responseContent)
@@ -73,7 +73,46 @@ describe('Package', function () {
             if (error) {
                 return done(error)
             }
-            done()
+            fs.unlink(pack.path, function (err) {
+                if (err) {
+                    return done(err)
+                }
+                fs.unlink(pack.pathSig, function (err) {
+                    if (err) {
+                        return done(err)
+                    }
+                    done()
+                })
+            })
+        })
+    })
+    it('download package corrupt', function (done) {
+        var baseURI = '/repo/x86_64/'
+        var responseContent = 'hola'
+        var opt = Object.assign({}, options)
+        opt.path = path.resolve('/tmp/')
+        opt.md5 = '4d186321c1a7f0f354b297e8924ab230'
+        opt.mirror = mirror + 'repo/x86_64'        
+        nock(mirror)
+            .get(baseURI + opt.filename)
+            .reply(200, responseContent)
+            .get(baseURI + opt.filename + '.sig')
+            .reply(200, responseContent)
+        const MockPackage = require('../src/package')
+        var pack = new MockPackage(packageName, opt)
+        pack.download(function (error, file) {
+            console.log(error)
+            if (error) {
+                try {
+                    assert.strictEqual(error.code, 'CORRUPT')
+                    return done()
+                } catch (err) {
+                    return done(err)
+                }
+            }
+            done(
+                new Error('Esperado un error por integridad del archivo')
+            )
         })
     })
 })
