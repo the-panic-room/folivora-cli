526d9064c212d06fc74d640a42b68604b60acf7a
Jhonny Mata <solucionesmatajm@gmail.com>
2019-08-25 20:25:06 -0400

    agregado prueba calcular suma de verificacion md5

--- test/package.spec.js
+++ test/package.spec.js
@@ -20,4 +20,22 @@ describe("Package", function () {
         assert.equal(pack.path, path.join(options.path, options.filename))
         assert.equal(pack.pathSig, path.join(options.path, options.filename + ".sig"))
     })
+    it("package checkSum", function (done) {
+        var options = {
+            version: "2.2.53-1",
+            filename: "acl-2.2.53-1-x86_64.pkg.tar.xz",
+            md5: "aaaea535e603f2b55cb320a42cc70397",
+            arch: "x86_64",
+            path: path.resolve("./test/repo/")
+        }
+        var pack = new Package("acl", options)
+        pack.checkSum(function (error, hash) {
+            if (error) {
+                done(error)
+            }
+            assert.equal(hash, pack.md5)
+            done()
+            // aqui va assert
+        })
+    })
 })
