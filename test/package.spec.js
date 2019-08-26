796c97aea36eeb8b1a3ce37f05b8e23f46564ff5
Jhonny Mata <solucionesmatajm@gmail.com>
2019-08-26 09:20:51 -0400

    aplicando buenas practicas pruebas package

--- test/package.spec.js
+++ test/package.spec.js
@@ -1,85 +1,74 @@
-var assert = require("assert"),
-    path = require("path"),
-    Package = require("../src/package"),
-    nock = require('nock')
+const assert = require('assert')
+const path = require('path')
+const Package = require('../src/package')
+const nock = require('nock')
 
-
-describe("Package", function () {
-    
-    it("new Package", function () {
-        var options = {
-            version: "2.2.53-1",
-            filename: "acl-2.2.53-1-x86_64.pkg.tar.xz",
-            md5: "aaaea535e603f2b55cb320a42cc70397",
-            arch: "x86_64",
-            path: path.resolve("./test/repo/")
-        }
-        var pack = new Package("acl", options)
-        assert.equal(pack.filename, options.filename)
-        assert.equal(pack.version, options.version)
-        assert.equal(pack.md5, options.md5)
-        assert.equal(pack.arch, options.arch)
-        assert.equal(pack.path, path.join(options.path, options.filename))
-        assert.equal(pack.pathSig, path.join(options.path, options.filename + ".sig"))
+describe('Package', function () {
+    var options = {
+        version: '2.2.53-1',
+        filename: 'acl-2.2.53-1-x86_64.pkg.tar.xz',
+        md5: 'aaaea535e603f2b55cb320a42cc70397',
+        arch: 'x86_64',
+        path: path.resolve('./test/repo/')
+    }
+    var packageName = 'acl'
+    var mirror = 'http://quantum-mirror.hu/mirrors/pub/manjaro/stable/'
+    it('new Package', function () {
+        var pack = new Package(packageName, options)
+        assert.strictEqual(pack.filename, options.filename)
+        assert.strictEqual(pack.version, options.version)
+        assert.strictEqual(pack.md5, options.md5)
+        assert.strictEqual(pack.arch, options.arch)
+        assert.strictEqual(pack.path, path.join(options.path, options.filename))
+        assert.strictEqual(pack.pathSig, path.join(options.path, options.filename + '.sig'))
     })
-    it("package checkSum", function (done) {
-        var options = {
-            version: "2.2.53-1",
-            filename: "acl-2.2.53-1-x86_64.pkg.tar.xz",
-            md5: "aaaea535e603f2b55cb320a42cc70397",
-            arch: "x86_64",
-            path: path.resolve("./test/repo/")
-        }
-        var pack = new Package("acl", options)
+    it('package checkSum', function (done) {
+        var pack = new Package(packageName, options)
         pack.checkSum(function (error, hash) {
             if (error) {
                 done(error)
             }
-            assert.equal(hash, pack.md5)
+            assert.strictEqual(hash, pack.md5)
             done()
         })
     })
-    it("package checkSum invalid not exist file", function (done) {
+    it('package checkSum invalid not exist file', function (done) {
         var options = {
-            version: "2.2.53-1",
-            filename: "acl-2.2.51-1-x86_64.pkg.tar.xz",
-            md5: "aaaea535e603f2b55cb320a42cc70397",
-            arch: "x86_64",
-            path: path.resolve("./test/repo/")
+            version: '2.2.53-1',
+            filename: 'acl-2.2.51-1-x86_64.pkg.tar.xz',
+            md5: 'aaaea535e603f2b55cb320a42cc70397',
+            arch: 'x86_64',
+            path: path.resolve('./test/repo/')
         }
-        var pack = new Package("acl", options)
+        var pack = new Package(packageName, options)
         pack.checkSum(function (error, hash) {
             if (error) {
                 try {
-                    assert.equal(error.code, "ENOENT")
+                    assert.strictEqual(error.code, 'ENOENT')
                     done()
-                } catch(error) {
+                } catch (error) {
                     done(error)
                 }
                 return
             }
-            done("Esperaba un error al no existir archivo")
+            done('Esperaba un error al no existir archivo')
         })
     })
-    it("download package", function (done) {
-        var mirror = "http://quantum-mirror.hu/mirrors/pub/manjaro/stable/"
+    it('download package', function (done) {
         var baseURI = '/repo/x86_64/'
-        var responseContent = "hola"
-        var options = {
-            version: "2.2.53-1",
-            filename: "acl-2.2.51-1-x86_64.pkg.tar.xz",
-            md5: "4d186321c1a7f0f354b297e8914ab240",
-            arch: "x86_64",
-            path: path.resolve("/tmp/"),
+        var responseContent = 'hola'
+        var opt = Object.assign({
+            path: path.resolve('/tmp/'),
+            md5: '4d186321c1a7f0f354b297e8914ab240',
             mirror: mirror + 'repo/x86_64'
-        }
+        }, options)
         nock(mirror)
-            .get(baseURI + options.filename)
+            .get(baseURI + opt.filename)
             .reply(200, responseContent)
-            .get(baseURI + options.filename + ".sig")
+            .get(baseURI + opt.filename + '.sig')
             .reply(200, responseContent)
-        const MockPackage = require("../src/package")
-        var pack = new MockPackage("acl", options)
+        const MockPackage = require('../src/package')
+        var pack = new MockPackage(packageName, opt)
         pack.download(function (error, file) {
             if (error) {
                 return done(error)
