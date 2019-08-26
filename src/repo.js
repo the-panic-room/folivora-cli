326f2a2e54ccd64579b39e87421491e0d89a76f5
Jhonny Mata <solucionesmatajm@gmail.com>
2019-08-26 07:57:00 -0400

    aplicando buenas practicas repo

--- src/repo.js
+++ src/repo.js
@@ -1,33 +1,34 @@
-const getInfo = require("./directory").getInfo,
-    File = require("./directory").File,
-    Package = require("./package"),
-    PackageList = require("./packagelist"),
-    path = require("path"),
-    tmp = require('tmp'),
-    fs = require('fs')
-
+const path = require('path')
+const fs = require('fs')
+const tmp = require('tmp')
+const directory = require('./directory')
+const getInfo = directory.getInfo
+const File = directory.File
+const Package = require('./package')
+const PackageList = require('./packagelist')
 
 class Repository {
-    constructor(name, options) {
+    constructor (name, options) {
         options = options || {}
         this.name = name
         this.path = options.path
         this.mirror = options.mirror
-        this.arch = options.arch || "any"
+        this.arch = options.arch || 'any'
         if (this.mirror) {
-            this.mirror = (this.mirror.match(/^https?:\/\//i)) ? this.mirror : "http://" + this.mirror
+            this.mirror = (this.mirror.match(/^https?:\/\//i)) ? this.mirror : 'http://' + this.mirror
         }
-        this.db_name = name + ".db.tar.gz"
+        this.db_name = name + '.db.tar.gz'
         if (!this.path) {
-            throw new Error("Es requerido un directorio valido")
+            throw new Error('Es requerido un directorio valido')
         }
     }
+
     updateDatabase () {
         var self = this
         return new Promise(function (resolve, reject) {
             var packageDB = new Package(self.name, {
                 arch: self.arch,
-                mirror: self.mirror + self.name + "/" + self.arch,
+                mirror: self.mirror + self.name + '/' + self.arch,
                 filename: self.db_name,
                 path: self.path
             })
@@ -40,7 +41,8 @@ class Repository {
             }, true)
         })
     }
-    read() {
+
+    read () {
         var self = this
         return new Promise(function (resolve, reject) {
             getInfo(self.path, function (err, info) {
@@ -50,82 +52,77 @@ class Repository {
                 resolve(info)
             })
         })
-        .then(function (data) {
-            if (!data.isDir()) {
-                throw new Error("El repositorio debe ser un directorio valido")
-            }
-            self.dir = data
-            return new Promise(function (resolve, reject) {
-                getInfo(path.join(self.dir.path, self.db_name), function (err, info) {
-                    if (err) {
-                        return reject(err)
-                    }
-                    resolve(info)
-                })
-            })
-        })
-        .then(function (db) {
-            self.db = db
-            var files = new PackageList()
-            return new Promise(function (resolve, reject) {
-                db.read()
-                .on("error", reject)
-                .on("entry", function (file) {
-                    file.collect().then(function (data) {
-                        var text = data.toString(),
-                            dir = file.path
-                        if (text) {
-                            var options = {
-                                version: text.match(/\%VERSION\%\n(.*)/i)[1],
-                                filename: text.match(/\%FILENAME\%\n(.*)/i)[1],
-                                md5: text.match(/\%MD5SUM\%\n(.*)/i)[1],
-                                arch: text.match(/\%ARCH\%\n(.*)/i)[1],
-                                path: self.path
-                            }
-                            options.mirror = self.mirror + self.name + "/" + options.arch
-                            files.push(
-                                new Package(
-                                    text.match(/\%NAME\%\n(.*)/i)[1],
-                                    options
-                                )
-                            )
-                        }
-                    })
-                })
-                .on("end", function () {
-                    tmp.file(function _tempFileCreated(err, path, fd, cleanupCallback) {
+            .then(function (data) {
+                if (!data.isDir()) {
+                    throw new Error('El repositorio debe ser un directorio valido')
+                }
+                self.dir = data
+                return new Promise(function (resolve, reject) {
+                    getInfo(path.join(self.dir.path, self.db_name), function (err, info) {
                         if (err) {
                             return reject(err)
                         }
-                        self.clean = cleanupCallback
-                        self.packages = files
-                        fs.writeFile(path, JSON.stringify(files.getAll()), function (err) {
-                            if (err) {
-                                return reject(err)
-                            }
-                            self.fileDB = new File(path)
-                            resolve(files)
-                        })
-                    });
+                        resolve(info)
+                    })
                 })
             })
-        })
+            .then(function (db) {
+                self.db = db
+                var files = new PackageList()
+                return new Promise(function (resolve, reject) {
+                    db.read()
+                        .on('error', reject)
+                        .on('entry', function (file) {
+                            file.collect().then(function (data) {
+                                var text = data.toString()
+                                if (text) {
+                                    var options = {
+                                        version: text.match(/%VERSION%\n(.*)/i)[1],
+                                        filename: text.match(/%FILENAME%\n(.*)/i)[1],
+                                        md5: text.match(/%MD5SUM%\n(.*)/i)[1],
+                                        arch: text.match(/%ARCH%\n(.*)/i)[1],
+                                        path: self.path
+                                    }
+                                    options.mirror = self.mirror + self.name + '/' + options.arch
+                                    files.push(
+                                        new Package(
+                                            text.match(/%NAME%\n(.*)/i)[1],
+                                            options
+                                        )
+                                    )
+                                }
+                            })
+                        })
+                        .on('end', function () {
+                            tmp.file(function _tempFileCreated (err, path, fd, cleanupCallback) {
+                                if (err) {
+                                    return reject(err)
+                                }
+                                self.clean = cleanupCallback
+                                self.packages = files
+                                fs.writeFile(path, JSON.stringify(files.getAll()), function (err) {
+                                    if (err) {
+                                        return reject(err)
+                                    }
+                                    self.fileDB = new File(path)
+                                    resolve(files)
+                                })
+                            })
+                        })
+                })
+            })
     }
-    getPackage(name) {
+
+    getPackage (name) {
         if (!this.packages) {
-            throw new Error("Debe cargar el repositorio primero. ejecute .read()")
+            throw new Error('Debe cargar el repositorio primero. ejecute .read()')
         }
         return this.packages.filter(function (row) {
             return row.name === name
-        })[0]        
+        })[0]
     }
-    foreach(callback) {
-        var self = this
-        return new Promise(function (resolve, reject) {
 
-        })
-    }
-    check() {
+    check () {
         var self = this
         var errors = []
         return self.packages.asyncForeach(function (pkg) {
@@ -143,31 +140,10 @@ class Repository {
                 })
             })
         })
-        .then(function () {
-            return errors
-        })
+            .then(function () {
+                return errors
+            })
     }
-}
-
-
-function _getPackage(obj, files, name) {
-    var isFile = files.filter(function (row) {
-        return row.name === name
-    })[0]
-    if (!isFile) {
-        return null
-    }
-    return new Promise(function (resolve, reject) {
-        getInfo(path.join(obj.path, isFile.filename), function (err, info) {
-            if (err) {
-                isFile.exist = false
-            } else {
-                isFile.exist = true
-                isFile.file = info
-            }
-            resolve(isFile)
-        })
-    })
 }
 
 module.exports = Repository
