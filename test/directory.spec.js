c20a6c1ab8596d906d2d09a68d77beff672a0f89
Jhonny Mata <solucionesmatajm@gmail.com>
2019-09-28 08:28:19 -0400

    pruebas de recorrido de directorios

--- test/directory.spec.js
+++ test/directory.spec.js
@@ -5,9 +5,9 @@ const Directory = require('../src/directory')
 
 describe('Directory', function () {
     // Pruebas de directorio.
-    it('loop path', function (done) {
+    it('loop path: good', function (done) {
         var dir = new ListDirectory(path.resolve('./example/'), ['./', './'])
-        var event = dir.forEach()
+        var event = dir.loop()
         event.on('data', function (data, next) {
             assert.ok(data instanceof Directory)
             next()
@@ -17,6 +17,69 @@ describe('Directory', function () {
         })
         event.on('error', function (err) {
             done(err)
+        })
+    })
+    // Pruebas de directorio callback.
+    it('loop path callback: good', function (done) {
+        var dir = new ListDirectory(path.resolve('./example/'), ['./', './'])
+        dir.loop(function (err, data) {
+            if (err) {
+                done(err)
+            }
+            assert.ok(data instanceof Directory)
+        }, done)
+    })
+
+    // Pruebas de directorio reemplazo.
+    it('loop path: good replace', function (done) {
+        var dir = new ListDirectory(path.resolve('./example/'), ['./', './'])
+        var event = dir.loop()
+        event.on('data', function (data, next) {
+            var index = dir.index
+            var replace = '/tmp'
+            assert.ok(data instanceof Directory)
+            next(null, replace)
+            assert.strictEqual(dir.getRawIndex(index), replace)
+        })
+        event.on('close', function () {
+            done()
+        })
+        event.on('error', function (err) {
+            done(err)
+        })
+    })
+
+    // Pruebas de directorio reemplazo callback.
+    it('loop path callback: good replace', function (done) {
+        var dir = new ListDirectory(path.resolve('./example/'), ['./', './'])
+        dir.loop(function (err, data) {
+            if (err) {
+                done(err)
+            }
+            assert.ok(data instanceof Directory)
+            return '/tmp'
+        }, function () {
+            for (var i = 0, n = dir.count(); i < n; i++) {
+                assert.strictEqual(dir.getRawIndex(i), '/tmp')
+            }
+            done()
+        })
+    })
+
+    // No existe el directorio
+    it('loop path: not exist', function (done) {
+        var dir = new ListDirectory(path.resolve('./example/'), ['./', './pe'])
+        var event = dir.loop()
+        event.on('data', function (data, next) {
+            assert.ok(data instanceof Directory)
+            next()
+        })
+        event.on('close', function () {
+            done('Debe arrojar error de apertura')
+        })
+        event.on('error', function (err) {
+            assert.strictEqual(err.code, 'ENOENT')
+            done()
         })
     })
 })
