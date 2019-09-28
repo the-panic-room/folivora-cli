db1ea141697c8f88ef44952f180acb44911986ae
Jhonny Mata <solucionesmatajm@gmail.com>
2019-09-28 13:28:30 -0400

    agregado documentacion de codigo infobase

--- src/infobase.js
+++ src/infobase.js
@@ -1,6 +1,17 @@
 const path = require('path')
 
+/**
+ * @class InfoBase
+ * @description Clase Base para gestionar archivos y directorios.
+ * @version 0.0.1
+ * @author Jhonny Mata
+ */
 class InfoBase {
+    /**
+     * @constructor
+     * @param {String} uri ruta del archivo o directoro.
+     * @param {Object} stat informacion adicional.
+     */
     constructor (uri, stat) {
         stat = stat || {}
         this.path = path.resolve(uri)
@@ -12,10 +23,22 @@ class InfoBase {
         this.size = stat.size
     }
 
+    /**
+     * @method
+     * @function isFile
+     * @description retorna true si el objeto es un archivo.
+     * @returns {Boolean}
+     */
     isFile () {
         return false
     }
 
+    /**
+     * @method
+     * @function isDir
+     * @description retorna true si el objeto es un directorio.
+     * @returns {Boolean}
+     */
     isDir () {
         return false
     }
