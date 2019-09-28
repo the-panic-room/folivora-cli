4da4d4bf9c296a5ea338d85e8d41860d048c6b67
Jhonny Mata <solucionesmatajm@gmail.com>
2019-09-28 09:30:49 -0400

    documentacion en Directory

--- src/directory.js
+++ src/directory.js
@@ -2,11 +2,29 @@ const fs = require('fs')
 const InfoBase = require('./infobase')
 const ListDirectory = require('./listdirectory')
 
+/**
+ * @class Directory
+ * @description Manejo de directorios.
+ * @version 0.0.1
+ * @author Jhonny Mata
+ */
 class Directory extends InfoBase {
+    /**
+     * @method
+     * @function isDir
+     * @description retorna true si el objeto es un directorio.
+     * @returns {Boolean}
+     */
     isDir () {
         return true
     }
 
+    /**
+     * @method
+     * @function list
+     * @description Listar los archivos y directorios enlazadas.
+     * @return {Promise<ListDirectory>}
+     */
     list () {
         var self = this
         return new Promise(function (resolve, reject) {
